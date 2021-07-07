6eb64b3884ad32a99b5c06d0675c2a5c03276d6f
SoumikNandi01 <soumik.ss.ml@gmail.com>
2021-07-08 03:04:47 +0530

    Added BoilerPlate Code

---
+++ src/main/java/com/userdatacontainerdao/UserDataContainerDao/UserDataContainerDaoApplication.java
@@ -0,0 +1,13 @@
+package com.userdatacontainerdao.UserDataContainerDao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDataContainerDaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDataContainerDaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/userdatacontainerdao/UserDataContainerDao/UserDataContainerDaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userdatacontainerdao.UserDataContainerDao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDataContainerDaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
